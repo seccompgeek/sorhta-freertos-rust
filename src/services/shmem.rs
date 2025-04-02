9f36615b9ecdc9dc01280f15bbe547e0232bff20
SSGeek <kayondomartin45@gmail.com>
2025-04-02 19:23:50 +0900

    added services

--- src/services/shmem.rs
+++ src/services/shmem.rs
@@ -1,4 +1,4 @@
-use core::{mem::{self, replace, size_of, size_of_val}, ptr::{self, slice_from_raw_parts_mut}, sync::atomic::{AtomicBool, AtomicPtr, AtomicU64, AtomicU8, Ordering}};
+use core::{mem::{self, replace, size_of, size_of_val}, ptr::{self, slice_from_raw_parts_mut}, sync::atomic::{AtomicBool, AtomicPtr, AtomicU32, AtomicU64, AtomicU8, Ordering}};
 
 
 pub const SHMEM_BASE: usize = 0xE200_0000;
@@ -6,33 +6,33 @@ pub const RESULT_BUFF_LENGTH: usize = 0x80;
 pub const NUM_REQUEST_CORES: usize = 0x7;
 pub const REQUEST_MEMORY_BASE: usize = SHMEM_BASE + (((mem::size_of::<Settings>() + size_of::<usize>()-1)/size_of::<usize>())) * size_of::<usize>();
 
-pub const REQUEST_NONE: u8 = 0;
-pub const REQUEST_VALID: u8 = 1;
-pub const REQUEST_TAKEN: u8 = 2;
-pub const REQUEST_COMPLETED: u8 = 3;
-pub const REQUEST_FAILED: u8 = 0xFF;
+pub const REQUEST_NONE: u32 = 0;
+pub const REQUEST_VALID: u32 = 1;
+pub const REQUEST_TAKEN: u32 = 2;
+pub const REQUEST_COMPLETED: u32 = 3;
+pub const REQUEST_FAILED: u32 = 0xFF;
 
-pub const REQUEST_WRITE: u8 = 1;
-pub const REQUEST_READ: u8 = 2;
+pub const REQUEST_WRITE: u32 = 1;
+pub const REQUEST_READ: u32 = 2;
 
 #[repr(C)]
 pub struct Request {
-    pub(crate) kind: u8,
+    pub(crate) kind: u32,
     pub(crate) buf_addr: usize,
     pub(crate) buf_size: usize,
     pub(crate) result: [u32; RESULT_BUFF_LENGTH],
-    pub(crate) status: AtomicU8,
+    pub(crate) status: AtomicU32,
 }
 
 #[repr(C)]
 pub struct Settings {
-    initialized: AtomicU8,
+    initialized: AtomicU32,
     request_memory_base_addr: usize,
 }
 
 impl Settings {
     pub fn initialize(&mut self) {
-        self.initialized = AtomicU8::new(1);
+        self.initialized = AtomicU32::new(1);
         self.request_memory_base_addr = REQUEST_MEMORY_BASE;
     }
 }
@@ -40,6 +40,6 @@ impl Settings {
 
 impl Request {
     pub fn initialize(&mut self) {
-        self.status = AtomicU8::new(REQUEST_NONE);
+        self.status = AtomicU32::new(REQUEST_NONE);
     }
 }
