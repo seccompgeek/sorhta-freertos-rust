941352cec30896a51cede79c2dda47c8ac741ce8
SSGeek <kayondomartin45@gmail.com>
2025-04-03 13:38:50 +0900

    added services

--- src/services/shmem.rs
+++ src/services/shmem.rs
@@ -4,7 +4,7 @@ use core::{mem::{self, size_of}, sync::atomic::{AtomicU32, AtomicU64, AtomicU8, 
 pub const SHMEM_BASE: usize = 0xE200_0000;
 pub const RESULT_BUFF_LENGTH: usize = 0x80;
 pub const NUM_REQUEST_CORES: usize = 0x7;
-pub const REQUEST_MEMORY_BASE: usize = SHMEM_BASE + (((mem::size_of::<Settings>() + size_of::<Request>() + size_of::<usize>()-1)/size_of::<usize>())) * size_of::<usize>();
+pub const REQUEST_MEMORY_BASE: usize = SHMEM_BASE + (((mem::size_of::<Settings>() + size_of::<usize>()-1)/size_of::<usize>())) * size_of::<usize>();
 
 pub const REQUEST_NONE: u32 = 0;
 pub const REQUEST_VALID: u32 = 1;
