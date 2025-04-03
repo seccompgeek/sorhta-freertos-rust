1ec2ef6a93aa7cce7f0e7dbbbf799938349cdca7
SSGeek <kayondomartin45@gmail.com>
2025-04-03 18:26:11 +0900

    added services

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -102,6 +102,7 @@ impl Worker {
                     
                     _ => {
                         // Invalid request type
+                        requests[index].kind = req.kind;
                         requests[index].result[0] = req.kind;
                         requests[index].status.store(REQUEST_FAILED, Ordering::Release);
                     }
